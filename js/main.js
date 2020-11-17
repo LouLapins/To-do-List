e5bba6d131878bf048542852b02fcafe43df9125
Lou Lapins <lou@Lous-MacBook-Air.local>
2020-11-17 15:25:44 +0100

    Refactor, change variable names, make functions global, add comments

--- js/main.js
+++ js/main.js
@@ -1,97 +1,12 @@
 window.onload = function() {
 
-    let listwrapper = document.createElement("div");
-    listwrapper.id = "listwrapper";
-    document.body.appendChild(listwrapper);
-
-    let heading = document.createElement("h2");
-    heading.innerHTML = "To do list:";
-    listwrapper.appendChild(heading);
-
-
-    let footer = document.createElement("footer");
-    footer.id = "footer";
-    document.body.appendChild(footer);
-
-    let inputwrapper = document.createElement("div");
-    inputwrapper.id = "inputwrapper";
-    footer.appendChild(inputwrapper);
-
-    let inputfield = document.createElement("input");
-    inputfield.id = "inputfield";
-    inputfield.setAttribute("type", "text");
-    inputfield.setAttribute("placeholder", "Add a task...")
-    inputwrapper.appendChild(inputfield);
-
-
-
-    let addbutton = document.createElement("button");
-    addbutton.id = "addbutton";
-    addbutton.innerHTML = "+";
-    inputwrapper.appendChild(addbutton);
-
-
-    let ulToDo = document.createElement("ul");
-    ulToDo.id = "todolist";
-    listwrapper.appendChild(ulToDo);
+    createHtmlElements();
 
     runLoop();
 
-    addbutton.addEventListener("click", addTask);
-
-
-
-    function runLoop() {
-        for (let i = 0; i < todos.length; i++) {
-            let liToDo = document.createElement("li");
-            liToDo.id = "liToDo";
-            liToDo.innerHTML = todos[i].task;
-            let deletebutton = document.createElement("button");
-            deletebutton.id = "deletebutton";
-            deletebutton.className = "fas fa-times";
-            ulToDo.appendChild(liToDo);
-
-            deletebutton.addEventListener("click", () => { deleteTask(todos[i]) });
-
-            liToDo.appendChild(deletebutton);
-        }
-    }
-
-    function deleteTask(todo) {
-        todo.deleted = true;
-        if (todo.deleted = true) {
-            todos.splice(0, 1);
-            todolist.innerHTML = "";
-            runLoop();
-        }
-    }
-
-    // function deleteTask(todo) {
-    //     todo.deleted = true;
-    //     if (todos[i].deleted = false) {
-    //         todos.push(todos[i]);
-    //         todolist.innerHTML = "";
-    //         runLoop();
-    //     }
-    // }
-
-    function addTask() {
-        let liToDo = document.createElement("li");
-        liToDo.innerText = inputfield.value;
-        let ulToDo = document.getElementById("todolist");
-        ulToDo.appendChild(liToDo);
-        inputfield.value = "";
-
-
-        let newtodo = new Todo(liToDo.innerText, false, false);
-        todos.push(newtodo);
-        todolist.innerHTML = "";
-        runLoop();
-
-    }
-
-
 }
+
+///////////////////////////// ARRAY OF TODOS as objects ///////////////////////////////
 
 class Todo {
     constructor(task, finished, deleted) {
@@ -124,5 +39,107 @@ todos.push(todo7);
 todos.push(todo8);
 
 
-
 console.log(todos);
+
+///////////// CREATING ALL HTML ELEMENTS THROUGH JS //////////////
+
+function createHtmlElements() {
+
+    let listWrapper = document.createElement("div");
+    listWrapper.id = "listWrapper";
+    document.body.appendChild(listWrapper);
+
+
+    let heading = document.createElement("h2");
+    heading.innerHTML = "To do list:";
+    listWrapper.appendChild(heading);
+
+
+    let footer = document.createElement("footer");
+    footer.id = "footer";
+    document.body.appendChild(footer);
+
+
+    let inputWrapper = document.createElement("div");
+    inputWrapper.id = "inputWrapper";
+    footer.appendChild(inputWrapper);
+
+
+    let ulTodos = document.createElement("ul");
+    ulTodos.id = "ulTodos";
+    listWrapper.appendChild(ulTodos);
+
+
+    let inputField = document.createElement("input");
+    inputField.id = "inputField";
+    inputField.setAttribute("type", "text");
+    inputField.setAttribute("placeholder", "Add a task...")
+    inputWrapper.appendChild(inputField);
+
+
+    let addButton = document.createElement("button");
+    addButton.id = "addButton";
+    addButton.innerHTML = "+";
+    addButton.addEventListener("click", addTask);
+    inputWrapper.appendChild(addButton);
+}
+
+//////////////////// CREATING LOOP DISPLAYING ARRAY AS LISTITEMS //////////////////
+
+function runLoop() {
+    for (let i = 0; i < todos.length; i++)
+
+    ////// (BOOLEAN FUNCTION TO HIDE DELETED TODOS) /////
+        if ((todos[i].deleted) == false) {
+        let liTodos = document.createElement("li");
+        liTodos.id = "liTodos";
+        liTodos.innerHTML = todos[i].task;
+        ulTodos.appendChild(liTodos);
+
+        ///// (CREATING DELETEBUTTON INSIDE EACH LISTITEM) /////
+        let deleteButton = document.createElement("button");
+        deleteButton.id = "deleteButton";
+        deleteButton.className = "fas fa-times";
+        deleteButton.addEventListener("click", () => { deleteTask(todos[i]) });
+        liTodos.appendChild(deleteButton);
+    }
+}
+
+///////////////////////// FUNCTION TO DELETE A TODO ///////////////////////////////
+
+// Splice Method //
+
+// function deleteTask(todo) {
+//     todo.deleted = true;
+//     if (todo.deleted = true) {
+//         todos.splice(0, 1);
+//         ulTodos.innerHTML = "";
+//         runLoop();
+//     }
+// }
+
+
+// Filter values in loop with boolean Method //
+
+function deleteTask(object) {
+    object.deleted = true;
+    ulTodos.innerHTML = "";
+    runLoop();
+}
+
+//////////////////////// FUNCTION TO ADD A NEW TODO ///////////////////////////////
+
+function addTask() {
+    let liTodos = document.createElement("li");
+    liTodos.innerText = inputField.value;
+    let ulTodos = document.getElementById("ulTodos");
+    ulTodos.appendChild(liTodos);
+    inputField.value = "";
+
+
+    let newTodo = new Todo(liTodos.innerText, false, false);
+    todos.push(newTodo);
+    ulTodos.innerHTML = "";
+    runLoop();
+
+}
